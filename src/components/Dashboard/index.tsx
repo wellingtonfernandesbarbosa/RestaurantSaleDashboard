d849a7fa80179bc5302d9a676a92616ce712d6d8
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-08-05 09:29:05 -0300

    Implement buttons of dashboard

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,9 +1,20 @@
 import styles from "./Dashboard.module.scss";
 
+const list = Array.from({ length: 100 }, (_, i) => i + 1);
+
 export default function Dashboard() {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Dashboard</h1>
+      <ul className={styles.container__list}>
+        {list.map((i) => (
+          <li key={i} className={styles.container__list__item}>
+            <a className={styles.container__list__item__link} href="#">
+              <div className={styles.container__list__item__link__content}>{i}</div>
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
